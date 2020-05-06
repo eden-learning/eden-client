008cc9b77dc50c06d1ad406f4437e44aee90b357
Tashi D Gyeltshen <tashidgyeltshen@hotmail.com>
2020-05-06 10:26:09 -0400

    style: add layout elements

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -4,29 +4,27 @@ import hero from '../assets/hero-page.svg';
 
 export default function HeroSection() {
   return (
-    <section>
-      <div>
-        <div>
-          <div>
-            <img src={logo} alt="eden logo" />
-            <p>Learning tailored for your child&apos;s needs</p>
-            <h2>Launching Summer 2020</h2>
-            <div className="input-group mb-3">
-              <input
-                type="text"
-                className="form-control"
-                placeholder="Enter Email"
-                aria-label="Recipient's email"
-              />
-              <div className="input-group-append">
-                <button className="btn btn-outline-secondary" type="button">
-                  Get Notified
-                </button>
-              </div>
+    <section className="container">
+      <div className="row">
+        <div className="col-sm">
+          <img src={logo} alt="eden logo" />
+          <p>Learning tailored for your child&apos;s needs</p>
+          <h2>Launching Summer 2020</h2>
+          <div className="input-group mb-3">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Enter Email"
+              aria-label="Recipient's email"
+            />
+            <div className="input-group-append">
+              <button className="btn btn-outline-secondary" type="button">
+                Get Notified
+              </button>
             </div>
           </div>
         </div>
-        <img src={hero} alt="eden logo" />
+        <img src={hero} alt="eden logo" className="col-sm" />
       </div>
       <h3>
         We&apos;re building a platform with the individual attention of a
