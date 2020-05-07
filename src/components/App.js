62a246f135140793aca6674c957ee5c17d733ae8
github username <github email address>
2020-05-07 10:09:32 -0500

    feat: add section three to App

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import HeroSection from './HeroSection';
 import SectionTwo from './SectionTwo';
+import SectionThree from './SectionThree';
 import '../styles/App.css';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
       <HeroSection />
       <SectionTwo />
+      <SectionThree />
     </div>
   );
 }
