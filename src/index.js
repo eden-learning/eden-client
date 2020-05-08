e63da5d031bb836b7af13ee3b1f00dc8ea4e4f3d
Tashi D Gyeltshen <tashidgyeltshen@hotmail.com>
2020-05-08 13:41:28 -0400

    feat: add hamburger menu

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.css';
 import './styles/index.css';
 import './styles/SectionTwo.css';
 import './styles/IconInfoBox.css';
