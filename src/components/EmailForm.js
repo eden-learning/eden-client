f5aa20849662ebb9c953db646519b4f5530e884d
Tashi D Gyeltshen <tashidgyeltshen@hotmail.com>
2020-05-11 14:45:30 -0400

    feature: show dialogue box when user clicks the get notified button

--- src/components/EmailForm.js
+++ src/components/EmailForm.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import $ from 'jquery';
 import logo from '../assets/eden-logo.svg';
 
 export default function EmailForm({ className }) {
+  const handleShow = () => {
+    $('#myModal').modal('show');
+  };
+
   return (
     <div className={className}>
       <img className="logo mt-4" src={logo} alt="eden logo" />
@@ -18,9 +23,43 @@ export default function EmailForm({ className }) {
           aria-label="Recipient's email"
         />
         <div className="input-group-append">
-          <button className="btn btn-outline-secondary" type="button">
+          <button
+            className="btn btn-outline-secondary"
+            type="button"
+            onClick={handleShow}
+          >
             Get Notified
           </button>
+
+          <div className="modal" id="myModal" tabIndex="-1" role="dialog">
+            <div className="modal-dialog" role="document">
+              <div className="modal-content">
+                <div className="modal-header">
+                  <h5 className="modal-title">Thank You!</h5>
+                  <button
+                    type="button"
+                    className="close"
+                    data-dismiss="modal"
+                    aria-label="Close"
+                  >
+                    <span aria-hidden="true">&times;</span>
+                  </button>
+                </div>
+                <div className="modal-body">
+                  <p>You will be notified when the app is launched.</p>
+                </div>
+                <div className="modal-footer">
+                  <button
+                    type="button"
+                    className="btn btn-secondary"
+                    data-dismiss="modal"
+                  >
+                    Close
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
