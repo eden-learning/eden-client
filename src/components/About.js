b0d2c1511624f750d5d8e807d3f8dc5d4535e6f2
github username <github email address>
2020-05-23 11:24:45 -0500

    Feat: Put the About Us block under the How To Help block

--- src/components/About.js
+++ src/components/About.js
@@ -6,21 +6,6 @@ export default function About() {
     <section className="about-section  container">
       <div className="row mb-1">
         <div className="col-2 col-md-1 box-violet">
-          <h5 className="vertical-box-header about">ABOUT US</h5>
-        </div>
-        <div id="about" className="col-10 col-md-11 row">
-          <div className="col-lg box box-red ">
-            We are a brilliant team from all over the world with one vision in
-            mind.
-          </div>
-          <div className="col-lg box box-yellow">
-            We are passionate creators, problem solvers, parents, teachers and
-            technologists!
-          </div>
-        </div>
-      </div>
-      <div className="row">
-        <div className="col-2 col-md-1 box-violet">
           <h5 className="vertical-box-header help">HOW TO HELP</h5>
         </div>
         <div id="help" className="col-10 col-md-11 row">
@@ -31,6 +16,21 @@ export default function About() {
           <div className="box box-green col-lg">
             <p>Teachers & parents</p>
             <p>Sign up for beta testing.</p>
+          </div>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-2 col-md-1 box-violet">
+          <h5 className="vertical-box-header about">ABOUT US</h5>
+        </div>
+        <div id="about" className="col-10 col-md-11 row">
+          <div className="col-lg box box-red ">
+            We are a brilliant team from all over the world with one vision in
+            mind.
+          </div>
+          <div className="col-lg box box-yellow">
+            We are passionate creators, problem solvers, parents, teachers and
+            technologists!
           </div>
         </div>
       </div>
