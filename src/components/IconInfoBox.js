85eb00303b281e90121dd2daeb6608b2c0b8a799
github username <github email address>
2020-05-23 11:35:28 -0500

    Feat: Add title to IconInfoBox component

--- src/components/IconInfoBox.js
+++ src/components/IconInfoBox.js
@@ -3,10 +3,11 @@ import PropTypes from 'prop-types';
 import '../styles/IconInfoBox.css';
 
 export default function IconInfoBox(props) {
-  const { icon, text } = props;
+  const { icon, title, text } = props;
   return (
     <div className="icon-info-box p-3 m-3">
       <img className="icon" src={icon} alt="ribbon" />
+      <p className="icon-text mt-5">{title}</p>
       <p className="icon-text mt-5">{text}</p>
     </div>
   );
@@ -15,4 +16,5 @@ export default function IconInfoBox(props) {
 IconInfoBox.propTypes = {
   text: PropTypes.string.isRequired,
   icon: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
 };
